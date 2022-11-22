bc3c602be71389d6a6ca1f66858611bd69ebf3e9
naimur <naimur202163@gmail.com>
2022-11-22 13:42:49 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import productsReducer, { productsFetch } from "./features/productsSlice";
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+});
+
+store.dispatch(productsFetch());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
